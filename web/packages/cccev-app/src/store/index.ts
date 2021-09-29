a20f5545f172b4ef6f063d83c1a9e056f1378835
Basile Savouret <basile@smartb.city>
2021-09-29 10:36:54 +0200

    default app added

---
+++ web/packages/cccev-app/src/store/index.ts
@@ -0,0 +1,9 @@
+import { initRedux } from "@smartb/g2-providers";
+
+const reducers = {
+}
+
+export const {store, history, reducer} = initRedux<typeof reducers>(reducers)
+
+export type State = ReturnType<typeof reducer>;
+
