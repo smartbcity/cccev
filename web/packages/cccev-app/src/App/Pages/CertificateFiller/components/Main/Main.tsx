4b84a67e70645b1962ee33ca4d314fb5a7872eb7
Basile Savouret <basile@smartb.city>
2021-09-30 11:38:53 +0200

    added accordion

--- web/packages/cccev-app/src/App/Pages/CertificateFiller/components/Main/Main.tsx
+++ web/packages/cccev-app/src/App/Pages/CertificateFiller/components/Main/Main.tsx
@@ -1,11 +1,37 @@
 import { Box } from "@mui/material"
 import { PageFilters } from "./PageFilters"
+import { CertificatFillerAccrodion, Category } from "components"
+import { useMemo } from "react"
 
 
 export const Main = () => {
+
+    const categories = useMemo((): Category[] => [
+        {
+            key: "certificatFiller-accordion-1",
+            name: "Category 1"
+        },{
+            key: "certificatFiller-accordion-2",
+            name: "Category 2"
+        },{
+            key: "certificatFiller-accordion-3",
+            name: "Category 3"
+        },{
+            key: "certificatFiller-accordion-4",
+            name: "Category 4"
+        },{
+            key: "certificatFiller-accordion-5",
+            name: "Category 5"
+        },{
+            key: "certificatFiller-accordion-6",
+            name: "Category 6"
+        }
+    ], [])
+
     return (
-        <Box sx={{padding: "10px 20px", paddingTop: "70px"}}>
+        <Box sx={{padding: "10px 20px", paddingTop: "70px", maxWidth: "1500px", margin: "auto"}}>
             <PageFilters />
+            <CertificatFillerAccrodion categories={categories} />
         </Box>
     )
 }
