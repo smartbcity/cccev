8a208bff15c8a515740ac40abe0f0dd55648cc53
Adrien Poupard <adrien@smartb.city>
2021-10-07 10:11:59 +0200

    Add cors configuration and fix docker build

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -6,11 +6,12 @@ plugins {
 }
 
 dependencies {
+    api ("org.springframework.boot:spring-boot-starter-security:2.1.3.RELEASE")
     api("city.smartb.f2:f2-spring-boot-starter-function-http:${Versions.f2}")
     implementation(project(":cccev-api"))
     implementation(project(":cccev-s2:request:request-app"))
 }
 
 tasks.withType<org.springframework.boot.gradle.tasks.bundling.BootBuildImage> {
-    imageName = "proto/cccev-gateway:${this.project.version}"
+    imageName = "smartbcity/cccev-gateway:${this.project.version}"
 }
